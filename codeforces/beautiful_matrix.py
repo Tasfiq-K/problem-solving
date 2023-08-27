d285347aa7b07a4d740d1104bfa5f1d0133e4395
Tasfiq Kamran <52997915+Tasfiq-K@users.noreply.github.com>
2023-08-27 02:16:37 +0000

    solved beautiful matrix problem

--- codeforces/beautiful_matrix.py
+++ codeforces/beautiful_matrix.py
@@ -1,12 +1,27 @@
 # 263A beautiful matrix
 
 
-def matrix(m: list):
-    pass
+def beautiful_matrix(matrix: list, mid: int):
+
+    """ function for the beautiful matrix problem """
+
+    for row, elem in enumerate(matrix):
+        for col in range(len(elem)):
+            if matrix[row][col] > 0:
+
+                return (abs(mid - row) + abs(mid - col))
 
 
 def main():
 
-    mat = [list(map(int,input().split())) for i in range(5)]
+    """ main function for the program """
 
-    print(mat)
+    n_rows = 5
+    mid = 2
+    mat = [list(map(int,input().split())) for i in range(n_rows)]
+    distance = beautiful_matrix(mat, mid)
+    print(distance)
+
+
+if __name__ == "__main__":
+    main()

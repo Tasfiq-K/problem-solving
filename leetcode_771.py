398e633208efa96943c83051d8875db6f9538e6c
Tasfiq Kamran <52997915+Tasfiq-K@users.noreply.github.com>
2023-10-03 06:28:08 +0000

    added solved leetcode 771 prolem

---
+++ leetcode_771.py
@@ -0,0 +1,16 @@
+# 771. Jewels and Stones
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        for jewel in jewels:
+          for stone in stones:
+              if jewel == stone:
+                count += 1
+        return count
+
+        # count = 0
+        # for jewel in stones:
+        #     if jewel in jewels:
+        #         count +=1
+        # return count

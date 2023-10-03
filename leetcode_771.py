11f2efe403b5b034c2b8b93caf65083d47f32c28
Tasfiq Kamran <52997915+Tasfiq-K@users.noreply.github.com>
2023-10-03 06:43:13 +0000

    edited 771

--- leetcode_771.py
+++ leetcode_771.py
@@ -4,9 +4,9 @@ class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         count = 0
         for jewel in jewels:
-          for stone in stones:
-              if jewel == stone:
-                count += 1
+            for stone in stones:
+                if jewel == stone:
+                    count += 1
         return count
 
         # count = 0

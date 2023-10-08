7ccd6bb11dda64a550d904a073ff5360b6bc03bc
tasfiq-k <tasfiq.kamran@gmail.com>
2023-10-09 02:07:57 +0600

    modified assignment_8.py

--- assignment_8.py
+++ assignment_8.py
@@ -13,15 +13,24 @@ class Solution:
         Returns: returns the index element who is single
         """
 
-        counter_dict = Counter(nums)
+        if len(nums) > 1:
 
-        for key, value in counter_dict.items():
-            # check if the value occurs only one for then return the corresponding key
-            # else return None
-            if value == 1:
-                return key
-        return None
+            counter_dict = Counter(nums)
+
+            for key, value in counter_dict.items():
+                # check if the value occurs only one for then return the corresponding key
+                # else return None
+                if value == 1:
+                    return key
+            return None
+        
+        elif len(nums) < 1:
+            return None
+        
+        return nums[0]
 
 sol = Solution()
 res = sol.SingleElement([3, 3, 1, 1, 1, 4, 4, 5])
+# res = sol.SingleElement([2])
+# res = sol.SingleElement([])
 print(res)

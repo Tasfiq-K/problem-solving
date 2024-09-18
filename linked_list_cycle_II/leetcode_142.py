6cc7d68ffd68e20f4272e22e9906a9ec2a08832f
tasfiq-k <tasfiq.kamran@gmail.com>
2024-09-19 03:15:45 +0600

    linked list cycle II, leetcode 142, solved

---
+++ linked_list_cycle_II/leetcode_142.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # initialize two pointer, fast and slow
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            # if they meet there's a cycle in the linked list
+            if slow == fast:
+                # reset slow if the two pointers meet to determine
+                # the start position of the cycle
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+
+                return slow
+        return None

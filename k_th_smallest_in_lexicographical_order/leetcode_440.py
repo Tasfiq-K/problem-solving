a2469ac6e5c3ce369aa249cdbcdb91f7183ef645
tasfiq-k <tasfiq.kamran@gmail.com>
2024-09-26 02:45:08 +0600

    daily challenge, september, k-th smallest in lexicographical order, leetcode 440, solved

---
+++ k_th_smallest_in_lexicographical_order/leetcode_440.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        curr = 1
+        k -= 1
+
+        while k > 0:
+            step = self._count_steps(n, curr, curr + 1) # n = 13, k = 2, curr = 1, cur
+            if step <= k:
+                curr += 1
+                k -= step
+            else:
+                curr *= 10
+                k -= 1
+        
+        return curr
+    
+    def _count_steps(self, n, prefix1, prefix2): # n = 13, p1 = 1, p2 = 2
+        steps = 0
+        while prefix1 <= n:
+            steps += min(n + 1, prefix2) - prefix1
+            prefix1 *= 10
+            prefix2 *= 10
+        
+        return steps

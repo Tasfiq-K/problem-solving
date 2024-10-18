c96c6b8ec4828834de025ccd1847d624bf430553
tasfiq-k <tasfiq.kamran@gmail.com>
2024-10-18 22:43:54 +0600

    solved boy or girl on codeforces

---
+++ codeforces/boy_or_girl.py
@@ -0,0 +1,9 @@
+def main():
+    # string = input()
+    if len(set(input())) % 2 == 0:
+        print("CHAT WITH HER!")
+    else:
+        print("IGNORE HIM!")
+
+if __name__ == "__main__":
+    main()

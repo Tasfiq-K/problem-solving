5eae3b4941b44277c58f001e0c5acfc175d813ed
tasfiq-k <tasfiq.kamran@gmail.com>
2024-10-18 22:46:16 +0600

    solved sum of three, codeforces

--- codeforces/sum_of_three.py
+++ codeforces/sum_of_three.py
@@ -1,31 +1,19 @@
+def sumOfThree():
 
+    for _ in range(int(input())):
+        n = int(input())
+        if n <= 6 or n == 9:
+            print("NO")
+            continue
+        
+        if (n - 3) % 3:
+            print("YES")
+            print(n - 3, 2, 1)
+        elif (n - 5) % 3:
+            print("YES")
+            print(n - 5, 4, 1)
+        else:
+            print("NO")
 
-numOfCases = int(input())
-
-cases = [ int(input()) for _ in range(numOfCases) ]
-
-for case in cases:
-    i = 1
-    non_divisible = []
-    while i != case:
-        if i % 3 != 0:
-            non_divisible.append(i)
-        i += 1
-    
-    found = []
-
-    for x in non_divisible:
-        for y in non_divisible[1:]:
-            for z in non_divisible[2:]:
-                if (x != y != z) and x + y + z == case:
-                    found.append((x, y, z))
-                    break
-
-
-    if len(found) > 0:
-        print("YES")
-        print(*found[0])
-    else:
-        print("NO")
-    
-
+if __name__ == "__main__":
+    sumOfThree()
